665e2bf52b99a575afca61e82d2cd52681f2d8ae
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-06-25 14:50:17 +0200

    add test

---
+++ turbopack/crates/turbopack-tests/tests/snapshot/basic/use-strict/output/4e721_crates_turbopack-tests_tests_snapshot_basic_use-strict_input_index_33d0b95d.js
@@ -0,0 +1,6 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "output/4e721_crates_turbopack-tests_tests_snapshot_basic_use-strict_input_index_33d0b95d.js",
+    {},
+    {"otherChunks":["output/4e721_crates_turbopack-tests_tests_snapshot_basic_use-strict_input_index_c0953835.js"],"runtimeModuleIds":["[project]/turbopack/crates/turbopack-tests/tests/snapshot/basic/use-strict/input/index.js [test] (ecmascript)"]}
+]);
+// Dummy runtime
